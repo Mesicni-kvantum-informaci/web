efe8c90a2a432ff3cb10065044ed6e2983d2e3b8
Vojtěch Kubrycht <vojtakubrycht@seznam.cz>
2023-09-22 06:54:46 +0200

    [css] fix lineheight, + task-frame, + task-image-grid, MathJax, [js] MathJax config, [test] + various testing

---
+++ js/mathjax-config.js
@@ -0,0 +1,9 @@
+window.MathJax = {
+    tex: {
+      inlineMath: [['\\(', '\\)'], ['$', '$']],
+      displayMath: [['\\eq{', '}'], ['$$', '$$'], ['\\[', '\\]']],
+    },
+    svg: {
+      fontCache: 'global'
+    }
+  };
